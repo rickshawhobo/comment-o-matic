961b6e786b992476273fdd157acc1d9ca18084b0
Guy Huynh <rickshawhobo@gmail.com>
2017-03-09 00:10:33 -0500

    adds the api doc

--- comments/app/Http/Controllers/CommentController.php
+++ comments/app/Http/Controllers/CommentController.php
@@ -9,12 +9,34 @@ use Illuminate\Http\Request;
 class CommentController extends Controller
 {
 
+
+    /**
+     * @api {get} /api/comments List comments
+     * @apiDescription Get all comments owned by the authenticated user
+     * @apiName ListComments
+     * @apiGroup Comments
+     *
+     * @apiSuccess {CommentObject} data The collection of found comments
+     */
+
     public function index(Request $request)
     {
 
         return $request->user()->comments;
 
     }
+
+    /**
+     * @api {post} /api/comments Create comments
+     * @apiDescription Create a new comment
+     * @apiName CreateComment
+     * @apiGroup Comments
+     *
+     * @apiParam {String} body The body of the comment
+     * @apiParam {String} tags The list of tags, separted by commas
+
+     * @apiSuccess {CommentObject} data The newly created comment object
+     */
 
     public function store(Request $request)
     {
@@ -38,6 +60,16 @@ class CommentController extends Controller
         }
         return $comment;
     }
+
+    /**
+     * @api {get} /api/comments/:id Fetch a comment
+     * @apiDescription Fetch a singular comment, requires the authenticated user to own the comment
+     * @apiName GetComment
+     * @apiGroup Comments
+     *
+     * @apiSuccess {CommentObject} data The found comment object
+     */
+
     public function show(Request $request, $commentId)
     {
 
@@ -48,4 +80,58 @@ class CommentController extends Controller
         return $comment;
 
     }
+
+    /**
+     * @api {delete} /api/comments/:id Delete a comment
+     * @apiDescription Delete a comment that's owned by the authenticated user
+     * @apiName DeleteComment
+     * @apiGroup Comments
+     *
+     * @apiSuccess {CommentObject} data The found comment object
+     */
+
+    public function destroy(Request $request, $commentId)
+    {
+
+        $comment = Comment::findOrFail($commentId);
+        if ($comment->user->id !== $request->user()->id) {
+            throw new AuthorizationException();
+        }
+        $comment->delete();
+
+        return $comment;
+    }
+
+    /**
+     * @api {patch} /api/comments/:id Update a comment
+     * @apiDescription Updates a comment that's owned by the authenticated user
+     * @apiName UpdateComment
+     * @apiGroup Comments
+     *
+     * @apiSuccess {CommentObject} data The found comment object
+     */
+    public function update(Request $request, $commentId)
+    {
+
+        $comment = Comment::findOrFail($commentId);
+        if ($comment->user->id !== $request->user()->id) {
+            throw new AuthorizationException();
+        }
+
+        if ($request->exists('tags')) {
+            $tagIds = [];
+
+            $tags = explode(",", $request->input('tags'));
+            foreach ($tags as $tag) {
+                $tagIds[] = Tag::firstOrCreate(['name' => $tag])->id;
+            }
+            $comment->tags()->sync($tagIds);
+        }
+
+        $comment->body = $request->input('body');
+        $comment->save();
+
+        return $comment;
+    }
+
 }
--- comments/app/Http/Controllers/UserController.php
+++ comments/app/Http/Controllers/UserController.php
@@ -8,6 +8,38 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
 
+
+
+    /**
+     * @api {post} /oauth/token Login
+     * @apiName Get a token
+     * @apiGroup User
+     *
+     * @apiParam {String} grant_type password
+     * @apiParam {String} client_id Client id
+     * @apiParam {String} client_secret client secret
+     * @apiParam {String} username The user's email
+     * @apiParam {String} password The user's password
+
+     * @apiSuccess {String} token_type Type of the token, Bearer
+     * @apiSuccess {String} access_token The JWT token used for authentication
+     * @apiSuccess {Number} expires_in Seconds in which the token will expire
+     * @apiSuccess {String} refresh_token The refresh token
+     */
+
+    /**
+     * @api {post} /api/users Create a new user
+     * @apiName CreateUser
+     * @apiGroup User
+     *
+     * @apiParam {String} email The email (username) must be unique and a valid email
+     * @apiParam {String} name Common name of the user
+     * @apiParam {String} password Password must be a minimum of 6 characters
+
+     * @apiSuccess {String} name name of the User.
+     * @apiSuccess {String} email email of the User.
+     */
+
     public function store(Request $request)
     {
         $this->validate($request, [
